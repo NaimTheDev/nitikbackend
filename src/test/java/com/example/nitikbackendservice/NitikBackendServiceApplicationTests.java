0e32820f7eac2db6e9ed8b751e37e139a4f47dda
unknown <naimabubaker@gmail.com>
2020-06-21 12:26:14 -0400

    added spring initializer

---
+++ src/test/java/com/example/nitikbackendservice/NitikBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nitikbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitikBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
